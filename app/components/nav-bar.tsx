e4ba8d4a524960b01e4cdb8a8145084d575131c0
Andrew <andrew.alan.sm@gmail.com>
2026-06-14 15:07:22 +0100

    Update nav bar to be more mobile friendly. Mobile 'burger menu' still uses a place-holder and isn't functional.

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -2,23 +2,26 @@ import { Link } from "react-router";
 
 export default function NavBar() {
   return (
-    <nav className="text-100 font-medium">
-      <ul className="flex gap-200 px-300 py-200">
+    <nav className="text-100 @container font-medium">
+      <ul className="flex gap-200">
         <li className="grow">
           <Link to="/">Andrew Smith</Link>
         </li>
-        <li>
+
+        <li className="@max-[40ch]:hidden">
           <Link to="/projects">Projects</Link>
         </li>
-        <li>
+        <li className="@max-[40ch]:hidden">
           <Link to="/about">About Me</Link>
         </li>
-        <li>
+        <li className="@max-[40ch]:hidden">
           <Link to="/contact">Contact</Link>
         </li>
-        <li>
+        <li className="@max-[40ch]:hidden">
           <Link to="/">GitHub</Link>
         </li>
+
+        <li className="hidden @max-[40ch]:block">Menu</li>
       </ul>
     </nav>
   );
