f9333374f844f739e16bcbcc9dc868de489b77e5
Andrew <andrew.alan.sm@gmail.com>
2026-06-18 22:36:14 +0100

    Update navigation bar to show current page as a different colour.

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,6 +1,8 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 export default function NavBar() {
+  const location = useLocation();
+
   return (
     <nav className="text-100 @container w-full font-medium">
       <ul className="flex gap-300">
@@ -8,13 +10,19 @@ export default function NavBar() {
           <Link to="/">Andrew Smith</Link>
         </li>
 
-        <li className="@max-[40ch]:hidden">
+        <li
+          className={`@max-[40ch]:hidden ${location.pathname === "/about" && "text-txt-link-active"}`}
+        >
           <Link to="/about">About Me</Link>
         </li>
-        <li className="@max-[40ch]:hidden">
+        <li
+          className={`@max-[40ch]:hidden ${location.pathname === "/projects" && "text-txt-link-active"}`}
+        >
           <Link to="/projects">Projects</Link>
         </li>
-        <li className="@max-[40ch]:hidden">
+        <li
+          className={`@max-[40ch]:hidden ${location.pathname === "/contact" && "text-txt-link-active"}`}
+        >
           <Link to="/contact">Contact</Link>
         </li>
         <li className="@max-[40ch]:hidden">
