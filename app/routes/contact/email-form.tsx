b387901cd3cb5d74874d1ad0b3dcf0400b1eab49
Andrew <andrew.alan.sm@gmail.com>
2026-07-29 20:01:04 +0100

    Make contact form compatible with Netlify forms.

--- app/routes/contact/email-form.tsx
+++ app/routes/contact/email-form.tsx
@@ -1,6 +1,6 @@
 export function EmailForm() {
   return (
-    <form className="grid gap-200">
+    <form name="contact" className="netlify grid gap-200">
       <Input name="name" />
       <Input name="email" />
       <Input name="subject" />
@@ -15,6 +15,7 @@ function Input({ name }: { name: string }) {
     <label>
       <span className="hidden">{name}</span>
       <input
+        type="text"
         placeholder={`${name}:`}
         className="bg-bg-link border-brdr-surface box-glow text-100 rounded-reg focus-visible:outline-brdr-link w-full justify-center border px-200 py-100 placeholder:capitalize focus-visible:outline-solid"
       />
@@ -38,7 +39,10 @@ function SubmitButton() {
   return (
     <label>
       <span className="hidden">Submit</span>
-      <button className="text-100 rounded-reg bg-bg-button font-button flex w-full cursor-pointer items-center justify-center py-100 text-center font-medium">
+      <button
+        type="submit"
+        className="text-100 rounded-reg bg-bg-button font-button flex w-full cursor-pointer items-center justify-center py-100 text-center font-medium"
+      >
         Submit
       </button>
     </label>
