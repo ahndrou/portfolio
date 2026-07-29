d50e01121dc0248152b9c02b39df109e9cb22686
Andrew <andrew.alan.sm@gmail.com>
2026-07-29 21:42:47 +0100

    Further updates to get Netlify forms working.

--- app/routes/contact/email-form.tsx
+++ app/routes/contact/email-form.tsx
@@ -1,23 +1,34 @@
 export function EmailForm() {
   return (
     <>
-      <form name="contact" data-netlify="true" className="grid gap-200">
-        <Input name="name" />
-        <Input name="email" />
-        <Input name="subject" />
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        className="grid gap-200"
+      >
+        {/* Necessary when using Netlify forms with client-rendered or SSR forms. */}
+        <input type="hidden" name="form-name" value="contact" />
+
+        <SimpleInput name="name" />
+        <SimpleInput name="email" />
+        <SimpleInput name="subject" />
+
         <MessageTextArea />
+
         <SubmitButton />
       </form>
     </>
   );
 }
 
-function Input({ name }: { name: string }) {
+function SimpleInput({ name }: { name: string }) {
   return (
     <label>
       <span className="hidden">{name}</span>
       <input
         type="text"
+        name={name}
         placeholder={`${name}:`}
         className="bg-bg-link border-brdr-surface box-glow text-100 rounded-reg focus-visible:outline-brdr-link w-full justify-center border px-200 py-100 placeholder:capitalize focus-visible:outline-solid"
       />
@@ -30,6 +41,7 @@ function MessageTextArea() {
     <label>
       <span className="hidden">Message</span>
       <textarea
+        name="message"
         placeholder="Message:"
         className="bg-bg-link border-brdr-surface box-glow text-100 rounded-reg focus-visible:outline-brdr-link h-[10rem] w-full justify-center border px-200 py-100 focus-visible:outline-solid"
       />
