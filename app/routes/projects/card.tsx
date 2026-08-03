7dc84e73725620b225c8afacb0b978ac7da1326a
Andrew <andrew.alan.sm@gmail.com>
2026-08-03 18:47:39 +0100

    Modify project card styling.

--- app/routes/projects/card.tsx
+++ app/routes/projects/card.tsx
@@ -17,31 +17,21 @@ export function Card({
   techList: string[];
 }) {
   return (
-    <article className="grid grid-rows-[auto_1fr] gap-x-400 gap-y-300 lg:gap-y-200">
+    <article className="grid grid-rows-[auto_1fr] gap-300">
       <ProjectImage src={imgSrc} />
-
-      <Heading heading={title} />
-
-      <div className="grid gap-100">
-        <div className="grid gap-100">
-          {paragraphs.map((content) => (
-            <p>{content}</p>
-          ))}
-        </div>
-
-        <div className="mbs-300 grid gap-300 lg:mbs-auto">
-          <TechList technologies={techList} />
-
-          <Links githubUrl={githubUrl} websiteUrl={websiteUrl} />
-        </div>
+      <div className="bg-bg-surface-2 border-outline-2 rounded-reg box-glow grid gap-300 border p-200">
+        <Heading heading={title} />
+        <ProjectBlurb paragraphs={paragraphs} />
       </div>
+      <TechList technologies={techList} />
+      <Links githubUrl={githubUrl} websiteUrl={websiteUrl} />
     </article>
   );
 }
 
 function Heading({ heading }: { heading: string }) {
   return (
-    <h2 className="text-400 text-txt-heading-2 font-heading leading-none">
+    <h2 className="text-400 text-txt-heading-2 font-heading leading-none font-medium">
       {heading}
     </h2>
   );
@@ -54,6 +44,16 @@ function ProjectImage({ src }: { src: string }) {
         className="block h-full w-full object-cover lg:object-top"
         src={src}
       />
+    </div>
+  );
+}
+
+function ProjectBlurb({ paragraphs }: { paragraphs: string[] }) {
+  return (
+    <div className="grid w-[80ch] gap-200">
+      {paragraphs.map((content) => (
+        <p>{content}</p>
+      ))}
     </div>
   );
 }
