ef13b7357183f071b8bcd6c4bf3f0c1926e30c36
Andrew <andrew.alan.sm@gmail.com>
2026-09-02 20:50:03 +0100

    Updating padding in tech list to improve alignment on project cards.

--- app/routes/projects/tech-list.tsx
+++ app/routes/projects/tech-list.tsx
@@ -1,6 +1,6 @@
 export default function TechList({ technologies }: { technologies: string[] }) {
   return (
-    <section className="grid gap-2 rounded-md p-3">
+    <section className="grid gap-2">
       <h3 className="text-text-quiet font-mono uppercase">Built with</h3>
 
       <ol className="flex flex-wrap gap-2">
@@ -16,7 +16,7 @@ function TechItem({ technology }: { technology: string }) {
   return (
     <li
       key={technology}
-      className="bg-fill border-line grow rounded-sm border px-2 py-1 text-center font-mono text-xs"
+      className="bg-fill border-line grow rounded-sm border p-2 text-center font-mono text-xs"
     >
       {technology}
     </li>
