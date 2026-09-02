f6b84efe1dcf3c49931b6c382e1de7cd7b27eac3
Andrew <andrew.alan.sm@gmail.com>
2026-09-02 23:46:09 +0100

    Fix center justification issue.

--- app/routes/layouts/main-layout.tsx
+++ app/routes/layouts/main-layout.tsx
@@ -3,9 +3,9 @@ import NavBar from "~/components/nav-bar";
 
 export default function MainLayout() {
   return (
-    <div className="grid content-start justify-center gap-7">
+    <div className="grid content-start gap-7">
       <NavBar />
-      <div className="grid max-w-5xl gap-7">
+      <div className="mx-auto grid max-w-5xl gap-7">
         <Outlet />
       </div>
     </div>
