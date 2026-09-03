cbeb986b39a3b55953126e31809a911444680428
Andrew <andrew.alan.sm@gmail.com>
2026-09-03 13:19:49 +0100

    Update contact route title.

--- app/routes/contact/route.tsx
+++ app/routes/contact/route.tsx
@@ -1,10 +1,9 @@
 import type { Route } from "./+types/route";
 import { EmailForm } from "./email-form";
-import { LinkedInSVG } from "./linked-in-svg";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Andrew Smith" },
+    { title: "Andrew Smith - Contact" },
     {
       name: "description",
       content: `Andrew Smith's web development portfolio. A collection of his projects, a description about him, as well as means to contact him can be found here.`,
