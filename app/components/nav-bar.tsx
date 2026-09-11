95ef4bf75f005c29b1950b6e26041a4ebea9ebbb
Andrew <andrew.alan.sm@gmail.com>
2026-09-11 13:10:55 +0100

    Fix nav bar wrapping on small screens.

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -10,32 +10,32 @@ export default function NavBar() {
   const location = useLocation();
 
   return (
-    <nav className="border-line @container sticky top-[0] w-full border-b py-4 text-sm backdrop-blur-2xl">
+    <nav className="border-line @container sticky top-[0] w-full border-b py-3 backdrop-blur-2xl min-[60ch]:py-4">
       <ul className="mx-auto flex max-w-5xl items-center gap-6 px-6 lg:px-0">
-        <li className="text-md grow">
-          <Link to="/" viewTransition>
+        <li className="grow">
+          <Link to="/" viewTransition className="text-md">
             Andrew Smith
           </Link>
         </li>
 
         {links.map(({ to, label }) => (
-          <li key={to}>
+          <li className="@max-[60ch]:hidden" key={to}>
             <Link
               to={to}
               viewTransition
-              className={`hover:text-text flex items-center rounded-md p-3 @max-[40ch]:hidden ${location.pathname === to ? "bg-fill text-text" : "text-text-muted"}`}
+              className={`hover:text-text flex items-center rounded-md p-3 ${location.pathname === to ? "bg-fill text-text" : "text-text-muted"}`}
             >
               {label}
             </Link>
           </li>
         ))}
 
-        <li>
+        <li className="@max-[60ch]:hidden">
           <a
             href="https://github.com/ahndrou"
             target="_blank"
             rel="noopener"
-            className="border-line-strong hover:bg-fill rounded-md border p-3 @max-[40ch]:hidden"
+            className="border-line-strong hover:bg-fill rounded-md border p-3"
           >
             <span className="after:ml-1 after:inline-block after:content-['↗']">
               GitHub
@@ -43,7 +43,7 @@ export default function NavBar() {
           </a>
         </li>
 
-        <li className="hidden @max-[40ch]:block">Menu</li>
+        <li className="hidden @max-[60ch]:block">Menu</li>
       </ul>
     </nav>
   );
