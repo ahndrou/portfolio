c512132ba3293e4516b8401eec23f4986117e4f6
Andrew <andrew.alan.sm@gmail.com>
2026-09-15 20:19:43 +0100

    Update spacing on project card.

--- app/routes/projects/card.tsx
+++ app/routes/projects/card.tsx
@@ -22,7 +22,7 @@ export function Card({
         <img className="block h-full w-full object-cover" src={imgSrc} />
       </div>
 
-      <div className="grid grid-cols-[2fr_1fr] items-start gap-5 rounded-md p-4 pt-6 *:col-span-2 md:p-6 md:*:col-span-1">
+      <div className="grid grid-cols-[10fr_6fr] items-start gap-x-6 gap-y-5 rounded-md p-4 pt-6 *:col-span-2 md:p-6 md:*:col-span-1">
         <div className="grid gap-5">
           <h2 className="font-display text-lg leading-none font-bold">
             {title}
