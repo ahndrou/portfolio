cf9e8798f8e213c1fed027e6d0af24ba694c55c1
Andrew <andrew.alan.sm@gmail.com>
2026-09-15 11:42:01 +0100

    Remove unnecessary function decomposition from card.
    It was just making the component harder to read rather than helping.

--- app/routes/projects/card.tsx
+++ app/routes/projects/card.tsx
@@ -18,42 +18,29 @@ export function Card({
 }) {
   return (
     <article className="surface border-line grid grid-rows-[auto_1fr] gap-4 overflow-clip rounded-lg border">
-      <ProjectImage src={imgSrc} />
+      <div className="max-h-[600px]">
+        <img
+          className="block h-full w-full object-cover lg:object-top"
+          src={imgSrc}
+        />
+      </div>
+
       <div className="grid grid-cols-[2fr_1fr] items-start gap-4 rounded-md p-6">
         <div className="grid gap-4">
-          <Heading heading={title} />
-          <ProjectBlurb paragraphs={paragraphs} />
+          <h2 className="font-display text-lg leading-none font-bold">
+            {title}
+          </h2>
+
+          <div className="text-text-muted grid gap-3">
+            {paragraphs.map((content) => (
+              <p>{content}</p>
+            ))}
+          </div>
+
           <Links githubUrl={githubUrl} websiteUrl={websiteUrl} />
         </div>
         <TechList technologies={techList} />
       </div>
     </article>
-  );
-}
-
-function Heading({ heading }: { heading: string }) {
-  return (
-    <h2 className="font-display text-lg leading-none font-bold">{heading}</h2>
-  );
-}
-
-function ProjectImage({ src }: { src: string }) {
-  return (
-    <div className="max-h-[600px]">
-      <img
-        className="block h-full w-full object-cover lg:object-top"
-        src={src}
-      />
-    </div>
-  );
-}
-
-function ProjectBlurb({ paragraphs }: { paragraphs: string[] }) {
-  return (
-    <div className="text-text-muted grid gap-3">
-      {paragraphs.map((content) => (
-        <p>{content}</p>
-      ))}
-    </div>
   );
 }
